b40363b5148d6c383ae17051f558147dabbf26d8
albertoborella <albertfborella@gmail.com>
2024-08-05 19:58:30 -0300

    agrego link en Post hacia todos los posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { useParams } from "react-router-dom"
+import { useParams, Link } from "react-router-dom"
 
 export const Post = () => {
     const param = useParams().id
     const post_id = Number(param)
     console.log(`corresponde al post ${post_id}`)
 
+    
+
   return (
-    <h2 style={{marginTop:"50px"}}>Corresponde al post {post_id}</h2>
+    <>
+    <h2 style={{marginTop:"50px"}}>Corresponde al post {post_id}</h2><br/>
+    <Link to="/posteos">Todos los posts</Link>
+    </>
   )
 }
