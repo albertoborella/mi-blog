c33d45eacbe9469d82be891a496af977a29f1456
albertoborella <albertfborella@gmail.com>
2024-09-06 11:20:21 -0300

    modificacion en NewPost

--- src/components/NewPost/NewPost.jsx
+++ src/components/NewPost/NewPost.jsx
@@ -10,8 +10,7 @@ const NewPost = ({agregarPost}) => {
 
     const handleSubmit = (e) => {
         const agregarNuevo = {
-            ID: inputValue,
-            usuario_id: inputValue,
+            ID: new Date().getTime(),
             nombre_usuario: inputValue,
             titulo: inputValue,
             texto: inputValue
@@ -26,14 +25,16 @@ const NewPost = ({agregarPost}) => {
     <form onSubmit={handleSubmit}>
         <label htmlFor="id">id:</label>
         <input type="text" id="id" name="id" value={inputValue} onChange={onInputChange}></input><br/>
-        <label htmlFor="usuario_id">Usuario_id:</label>
-        <input type="text" id="usuario_id" name="usuario_id" value={inputValue} onChange={onInputChange}></input><br/>
+        
         <label htmlFor="nombre_usuario">Nombre de usuario:</label>
         <input type="text" id="nombre_usuario" name="nombre_usuario" value={inputValue} onChange={onInputChange}></input><br/>
+        
         <label htmlFor="titulo">Titulo:</label>
         <input type="text" id="titulo" name="titulo" value={inputValue} onChange={onInputChange}></input><br/>
+        
         <label htmlFor="texto">Texto:</label>
         <textarea id="texto" name="texto" rows={8} value={inputValue} onChange={onInputChange}></textarea><br/>
+        
         <button type="submit">Agregar Post</button>
     </form>
     </>
--- src/components/Posteos/Posteos.jsx
+++ src/components/Posteos/Posteos.jsx
@@ -11,7 +11,6 @@ export const Posteos = () => {
       titulo: "Spiderman",
       texto:
         "Joven héroe con poderes arácnidos que combate el crimen balanceándose por Nueva York.",
-      user_id: 7,
       usuario: "stan_lee",
     },
     {
@@ -19,7 +18,6 @@ export const Posteos = () => {
       titulo: "X-MEN",
       texto:
         "Equipo de mutantes con habilidades únicas que luchan por la coexistencia pacífica entre humanos y mutantes.",
-      user_id: 7,
       usuario: "stan_lee",
     },
     {
@@ -27,7 +25,6 @@ export const Posteos = () => {
       titulo: "Capitán América",
       texto:
         "Soldado mejorado con un escudo indestructible que lidera a los Vengadores en la defensa del mundo.",
-      user_id: 7,
       usuario: "stan_lee",
     },
     {
@@ -35,7 +32,6 @@ export const Posteos = () => {
       titulo: "Thanos",
       texto:
         "Titán obsesionado con el poder y las Gemas del Infinito, busca la aniquilación para equilibrar el universo.",
-      user_id: 7,
       usuario: "stan_lee",
     },
     {
@@ -43,7 +39,6 @@ export const Posteos = () => {
       titulo: "Ant-Man",
       texto:
         "Héroe capaz de encogerse y controlar insectos con un traje especial para combatir el mal.",
-      user_id: 7,
       usuario: "stan_lee",
     },
   ];
